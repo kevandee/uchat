38d1c73f30e03b116ca1fbb40b4bfd268cf2dbab
RomanLytvynov <litvinromeo@gmail.com>
2022-01-19 15:56:50 +0200

    original buttons in headerbar

--- client/src/swiftchat_headerbar.c
+++ client/src/swiftchat_headerbar.c
@@ -17,19 +17,22 @@ void chat_decorate_headerbar()
     t_screen.headerbar = gtk_header_bar_new();
     gtk_header_bar_set_show_title_buttons (GTK_HEADER_BAR( t_screen.headerbar), FALSE);
     GtkWidget *headerbar_button1 = gtk_image_new_from_file("client/media/close.png");
-    gtk_widget_set_size_request(headerbar_button1, 5, 5);
+    gtk_widget_set_name(GTK_WIDGET(headerbar_button1), "close");
     g_signal_connect_swapped(click_close, "pressed", G_CALLBACK(gtk_window_destroy), t_screen.main_window);
     gtk_widget_add_controller(headerbar_button1, GTK_EVENT_CONTROLLER(click_close));
     gtk_header_bar_pack_start (GTK_HEADER_BAR( t_screen.headerbar), headerbar_button1);
     GtkWidget *headerbar_button2 = gtk_image_new_from_file("client/media/minimize.png");
-    gtk_widget_set_size_request(headerbar_button2, 5, 5);
+    gtk_widget_set_name(GTK_WIDGET(headerbar_button2), "minimize");
     g_signal_connect_swapped(click_minimize, "pressed", G_CALLBACK(gtk_window_minimize), t_screen.main_window);
     gtk_widget_add_controller(headerbar_button2, GTK_EVENT_CONTROLLER(click_minimize));
     gtk_header_bar_pack_start (GTK_HEADER_BAR( t_screen.headerbar), headerbar_button2);
     GtkWidget *headerbar_button3 = gtk_image_new_from_file("client/media/notworking.png");
-    gtk_widget_set_size_request(headerbar_button3, 5, 5);
+    gtk_widget_set_name(GTK_WIDGET(headerbar_button3), "maximize");
     gtk_header_bar_pack_start (GTK_HEADER_BAR( t_screen.headerbar), headerbar_button3);
     load_css_headerbar(t_screen.headerbar);
+    load_css_headerbar(headerbar_button1);
+    load_css_headerbar(headerbar_button2);
+    load_css_headerbar(headerbar_button3);
     gtk_window_set_titlebar (GTK_WINDOW(t_screen.main_window), t_screen.headerbar);
 }
 
